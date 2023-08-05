27191ab5278a1388cea5d39dc2b9463c9c892d38
Daniel Blasina <blzk100@gmail.com>
2023-08-05 13:23:00 +0200

    feat: added first failing test

---
+++ Core/PrimeService_IsPrimeShould.test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Prime.UnitTests.Services
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            Assert.IsFalse(true, "1 should not be prime");
+        }
+    }
+}
