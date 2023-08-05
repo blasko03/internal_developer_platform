368a670f40d0d0a12d9627d879fb48cf9e9cf1da
Daniel Blasina <blzk100@gmail.com>
2023-08-05 10:26:05 +0200

    feat: added backend implementation

---
+++ Backend/Controllers/CommitsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("api/commits")]
+public class CommitsController : ControllerBase
+{
+    /*
+    private readonly ILogger<CommitsController> _logger;
+
+    public CommitsController(ILogger<CommitsController> logger)
+    {
+        _logger = logger;
+    }
+    */
+    [HttpGet("get-commits")]
+    public Task<Core.Code.ICommit[]> Get()
+    {
+        return new Core.Code.Github.Commits().GetCommits(owner: "nodejs", repo: "node");
+    }
+}
