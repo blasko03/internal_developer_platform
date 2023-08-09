f34134c9e911baa5b5b667a68be59ac219fb0246
Daniel Blasina <blzk100@gmail.com>
2023-08-09 21:49:18 +0200

    feat: changed tests to xunit

--- Core/Code/Github/Commits.test.cs
+++ Core/Code/Github/Commits.test.cs
@@ -1,21 +1,19 @@
 ﻿using AppRunner.Core.Code.Github;
 using NSubstitute;
-using NUnit.Framework;
-
+using Xunit;
 namespace AppRunner.Core.UnitTests.Commits;
 
-[TestFixture]
 public class CallGetCommits
 {
     private static Task<string> TestData()
     {
         return File.ReadAllTextAsync(Path.Combine("test_files", "get_commits.json"));
     }
-    [Test]
+    [Fact]
     public async Task GetsCommitsFromService()
     {
         var httpClientMock = Substitute.For<IHttpClientWrapper>();
         httpClientMock.GetStringAsync(default!).ReturnsForAnyArgs(TestData());
-        Assert.IsTrue((await new Code.Github.Commits(httpClientMock).GetCommits("nodejs", "node")).Length > 0, "Should get an array of commits");
+        Assert.True((await new Code.Github.Commits(httpClientMock).GetCommits("nodejs", "node")).Length > 0, "Should get an array of commits");
     }
 }
--- Core/Code/Github/Pipelines.test.cs
+++ Core/Code/Github/Pipelines.test.cs
@@ -1,10 +1,9 @@
 ﻿using AppRunner.Core.Code.Github;
 using NSubstitute;
-using NUnit.Framework;
+using Xunit;
 
 namespace AppRunner.Core.UnitTests.Pipelines;
 
-[TestFixture]
 public class CallGetPipelines
 {
     private static Task<string> TestData()
@@ -12,11 +11,11 @@ public class CallGetPipelines
         return File.ReadAllTextAsync(Path.Combine("test_files", "get_pipelines.json"));
     }
 
-    [Test]
+    [Fact]
     public async Task GetsPipelinesFromService()
     {
         var httpClientMock = Substitute.For<IHttpClientWrapper>();
         httpClientMock.GetStringAsync(default!).ReturnsForAnyArgs(TestData());
-        Assert.IsTrue((await new Code.Github.Pipelines(httpClientMock).GetPipelines("nodejs", "node")).Length > 0, "Should get an array of pipelines");
+        Assert.True((await new Code.Github.Pipelines(httpClientMock).GetPipelines("nodejs", "node")).Length > 0, "Should get an array of pipelines");
     }
 }
